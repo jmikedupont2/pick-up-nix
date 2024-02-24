e0ece14d3e2603257e1a8b1602d4aa9918415d7f
mike dupont <mike.dupont@introspector.local>
2024-02-23 19:37:53 -0500

    config nix

--- shell.nix
+++ shell.nix
@@ -1,12 +1,12 @@
-let
+slet
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 
 pkgs.mkShellNoCC {
   packages = with pkgs; [
-  emacs
   opam
+  git 
   coq
       ((emacsPackagesFor pkgs.emacs).emacsWithPackages (
       epkgs: [
@@ -19,5 +19,4 @@ pkgs.mkShellNoCC {
 
 ];
 }
-
 
