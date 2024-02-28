f807678cd9b88a39837bf0628520b6372e144c43
mike dupont <mike.dupont@introspector.local>
2024-02-28 13:46:24 -0500

    update

---
+++ .config/nix/configuration.nix
@@ -0,0 +1,9 @@
+{
+  services.emacs.package = pkgs.emacs-unstable;
+
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+    }))
+  ];
+}
