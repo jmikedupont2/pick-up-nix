e09a562cf064c728663293015a56249243951727
mike <h@solfunmeme.com>
2025-09-06 20:22:15 +0000

    adding config

---
+++ new/config/nix-on-droid/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Basic example of Nix-on-Droid system config.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05"; # Stable channel
+    nix-on-droid = {
+      url = "github:nix-community/nix-on-droid/release-24.05"; # Stable channel
+      inputs.nixpkgs.follows = "nixpkgs";
+	};
+  };
+
+  outputs = { self, nixpkgs, nix-on-droid }: {
+
+    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+      pkgs = import nixpkgs { system = "aarch64-linux"; };
+      modules = [ ./nix-on-droid.nix ];
+    };
+
+  };
+}
---
+++ new/config/nix-on-droid/nix-on-droid.nix
@@ -0,0 +1,73 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Simply install just the packages
+  environment.packages = with pkgs; [
+    # User-facing stuff that you really really want to have
+    vim
+    git
+    #gemini-cli
+    emacs
+#    llvm
+#    ocaml
+#    coq
+#    haskellPackages.ghc
+##    metacoq
+#    guix
+#    emacsPackages.vterm
+    rustc
+    cargo
+#    gauche
+#    guile
+#    sops
+    tmux
+#    minizinc
+#    python3
+#    python3Packages.numpy
+#    python3Packages.scipy
+#    python3Packages.pandas
+#    python3Packages.scikit-learn
+#    python3Packages.tensorflow-lite
+#    python3Packages.pytorch
+#    gcc
+#    gdb
+    clang
+#    lldb
+    pkg-config
+    cmake
+    gh
+
+    # Some common stuff that people expect to have
+    procps
+    killall
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    hostname
+    man
+    gnugrep
+    gnupg
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
+  ];
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
+
+  # Read the changelog before changing this value
+  system.stateVersion = "24.05";
+
+  # Set up nix for flakes
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  # Set your time zone
+  #time.timeZone = "Europe/Berlin";
+}
