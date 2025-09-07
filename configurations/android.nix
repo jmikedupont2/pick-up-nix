e83e6eaca43ea3c9d0fbb82f5cafc36d68b30e2b
mike <h@solfunmeme.com>
2025-09-07 02:37:49 +0000

    Fix: Remove desktop-oriented packages from android.nix

--- configurations/android.nix
+++ configurations/android.nix
@@ -60,26 +60,7 @@
     unzip
 
     # Packages from nix-env -q (non-xorg)
-    adwaita-icon-theme
-    atk
-    at-spi2-atk
-    at-spi2-core
-    cairo
-    dconf
-    fontconfig
-    freetype
-    gdk-pixbuf
-    glib
-    gnome-themes-extra
-    gtk3
-    harfbuzz
-    hicolor-icon-theme
-    libepoxy
-    libthai
-    libxkbcommon
-    ncurses
-    pango
-    pixman
+    
 
     # To include optional xorg packages, uncomment the following line:
     # (import ./optional-xorg-packages.nix { inherit pkgs; })
