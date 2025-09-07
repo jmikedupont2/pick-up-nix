3527adb634b0e115d592b257e2ca5b30e4ec4e3f
mike <h@solfunmeme.com>
2025-09-07 02:39:32 +0000

    Fix: Minimize android.nix to basic structure

--- configurations/android.nix
+++ configurations/android.nix
@@ -3,67 +3,8 @@
 {
   # Simply install just the packages
   environment.packages = with pkgs; [
-    # User-facing stuff that you really really want to have
-    vim
-    git
-    gemini-cli
-    emacs
-    llvm_18
-    ocaml
-    coq
-    ghc
-    coqPackages.metacoq
-    guix
-    emacsPackages.vterm
-    rustc
-    cargo
-    gauche
-    guile
-    sops
-    tmux
-    minizinc
-#    python3
-#    python3Packages.numpy
-#    python3Packages.scipy
-#    python3Packages.pandas
-#    python3Packages.scikit-learn
-#    python3Packages.tensorflow-lite
-#    python3Packages.pytorch
-    #    gcc
-    gdb
-    clang
-    lldb
-    pkg-config
-    cmake
-    gh
-    shellcheck
-    asciinema
-    curl
-
-    # Some common stuff that people expect to have
-    procps
-    killall
-    diffutils
-    findutils
-    utillinux
-    tzdata
-    hostname
-    man
-    gnugrep
-    gnupg
-    gnused
-    gnutar
-    bzip2
-    gzip
-    xz
-    zip
-    unzip
-
-    # Packages from nix-env -q (non-xorg)
-    
-
-    # To include optional xorg packages, uncomment the following line:
-    # (import ./optional-xorg-packages.nix { inherit pkgs; })
+    # Keep a minimal package to ensure it builds
+    hello
   ];
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
