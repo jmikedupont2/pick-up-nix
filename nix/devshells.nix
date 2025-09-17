72d72741f1819e6059a9a96f07e48d628d8fbef6
mike <h@solfunmeme.com>
2025-09-17 18:58:11 +0000

    fix: Embed shellHook content in nix/devshells.nix
    Embeds the shellHook content directly into nix/devshells.nix to resolve
    "No such file or directory" error during nix develop.

--- nix/devshells.nix
+++ nix/devshells.nix
@@ -13,7 +13,15 @@
         unstablePkgs.rustc
         unstablePkgs.cargo
       ];
-      shellHook = builtins.readFile ./shell_hook.sh;
+      shellHook = ''
+        #!/usr/bin/env bash
+
+        echo "Current PATH: $PATH"
+        echo "Contents of unstablePkgs.cargo/bin: $(ls ${unstablePkgs.cargo}/bin)"
+        # No need to export PATH again, it's already there from buildInputs
+        echo "Attempting to run cargo check..."
+        bash -c "cargo check"
+      '';
     };
   });
 }
