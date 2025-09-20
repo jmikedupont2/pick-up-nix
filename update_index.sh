60cb134064acddbc6aaa338d00b65a9ef236cd6a
mike <h@solfunmeme.com>
2025-09-20 13:22:31 +0000

    feat(main): CRQ-016 - Update main repository after submodule changes

--- update_index.sh
+++ update_index.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-find . > data/files.txt
-find . -type f -name \*.md  > data/md.txt
-find . -type f -name \*.rs  > data/rs.txt
-find . -type f -name \*.mnz > data/mzn.txt
-grep -i Cargo.toml data/files.txt > data/rust_cargo.txt
+find . > file.txt
+#find . -type f -name \*.md  > data/md.txt
+#find . -type f -name \*.rs  > data/rs.txt
+#find . -type f -name \*.mnz > data/mzn.txt
+#grep -i Cargo.toml data/files.txt > data/rust_cargo.txt
