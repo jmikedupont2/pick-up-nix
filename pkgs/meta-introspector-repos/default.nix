9f3c1ddf43030d37e1ff1314b2935924472c1f87
mike <h@solfunmeme.com>
2025-09-21 16:41:04 +0000

    feat(CRQ-018): Add meta-introspector-repos Nix package

---
+++ pkgs/meta-introspector-repos/default.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.stdenv.mkDerivation {
+  pname = "meta-introspector-repos-json";
+  version = "0.1.0";
+
+  # We don't need any source code for this, as it generates data
+  # However, we need to ensure gh and jq are available in the build environment
+  buildInputs = [ pkgs.gh pkgs.jq ];
+
+  # The actual command to generate the JSON file
+  buildPhase = ''
+    mkdir -p $out
+    gh repo list meta-introspector --json name,url --limit 1000 > $out/github_meta-introspector_repos.json
+  '';
+
+  # No install phase needed as the buildPhase already puts the file in $out
+  installPhase = "";
+}
