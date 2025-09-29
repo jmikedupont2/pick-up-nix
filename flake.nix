a820da5f82eff5c8b6117e08f1c23e39166c3b28
mike <h@solfunmeme.com>
2025-09-29 19:37:54 +0000

    refactor: Remove commented-out today_concepts references from main flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     flake-utils.url = "github:meta-introspector/flake-utils?ref=feature/CRQ-016-nixify";
     naersk.url = "github:meta-introspector/naersk?ref=feature/CRQ-016-nixify";
     my-new-flake.url = "./nix/flakes/my-new-flake";
-#    today_concepts.url = "github:meta-introspector/time-2025?dir=source/github/meta-introspector/streamofrandom/2025/09/23/nix_concepts_and_facts&ref=feature/808017424794512875886459904961710757005754368000000000";
 #    template-generator-bin.url = "./tools/template_generator_bin"; # Keep this input
   };
 
@@ -36,12 +35,9 @@
           packages = { # Re-add the packages section
             #template-generator-bin = template-generator-bin.packages.${system}.default;
             meta-introspector-repos = import ./pkgs/meta-introspector-repos { inherit pkgs; };
-            number-23 = today_concepts.packages.${system}.number-23;
-            is-prime-23 = today_concepts.packages.${system}.is-prime-23;
-            fact-23-oracle = today_concepts.packages.${system}.fact-23-oracle;
-            ai-context-23 = today_concepts.packages.${system}.default; # ai-context-23 is the default package in the new flake
-            my-new-flake = my-new-flake.packages.${system}.default;
-  #          default = self.packages.${system}.template-generator-bin; # Set default to it
+          log-analyzer = logAnalyzer;
+          my-new-flake = my-new-flake.packages.${system}.default;
+#          default = self.packages.${system}.template-generator-bin; # Set default to it
           };
   
           devShells.default = pkgs.mkShell {
